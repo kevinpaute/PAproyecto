8b2ef0f1ba029d945d42dc8c4d2b2343768bcd26
dbgonzalez1 <65975855+dbgonzalez1@users.noreply.github.com>
2023-08-30 22:34:09 -0500

    Alerta Formato Imagen

--- Proyecto/src/app/imagen/imagen.component.ts
+++ Proyecto/src/app/imagen/imagen.component.ts
@@ -19,6 +19,18 @@ export class ImagenComponent implements OnInit{
 
   onFileSelected(event: any) {
     this.imagenSeleccionada = event.target.files[0] as File;
+
+    if (this.imagenSeleccionada && this.imagenSeleccionada.type === 'image/png') {
+      this.imagenSeleccionada = this.imagenSeleccionada;
+    } else {
+      Swal.fire({
+        title: 'Error',
+        text: 'Por favor, selecciona una imagen en formato PNG.',
+        icon: 'error'
+      });
+      this.imagenSeleccionada = null;
+    }
+
   }
 
   async subirImagen() {

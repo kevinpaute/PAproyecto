af8cf570e424b23b816178c89f3935c4fe5672aa
Jhon Zambrano Macias <75482142+JhonZambranoM@users.noreply.github.com>
2023-08-30 22:53:40 -0500

    Correcion Errores VerificacionComponent

--- Proyecto/src/app/verification/verification.component.ts
+++ Proyecto/src/app/verification/verification.component.ts
@@ -1,24 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
 import { MatTableDataSource } from '@angular/material/table';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { UsuarioService } from '../usuario.service';
 
 
-export interface Persona {
-  nombres: string;
-  apellidos: string;
-  imagen: string;
-}
-
-const PERSONAS: Persona[] = [
-  {
-    nombres: 'John',
-    apellidos: 'Doe',
-    imagen: 'https://picsum.photos/200/300'
-  },
-  // Agrega más objetos de persona según necesites
-];
 
 
 
@@ -28,65 +13,15 @@ const PERSONAS: Persona[] = [
   styleUrls: ['./verification.component.css']
 })
 export class VerificationComponent implements OnInit {
-  dataSource = new MatTableDataSource<Persona>(PERSONAS);
-  displayedColumns: string[] = ['nombres', 'apellidos', 'imagen','acciones'];
-
-  personas: Persona[] = [];
-  
-
-  ngOnInit(): void {
-    this.personaService.getPersona().then(
-      (data) => {
-        this.personas = data; // Asigna los datos recibidos a la propiedad personas
-      },
-      (error) => {
-        console.log('Error al obtener las personas:', error);
-      }
-    );
-    throw new Error('Method not implemented.');
-  }
+ 
 
 
-
-
-  aceptarPersona(persona: Persona) {
-    // Lógica para aceptar la persona
-  }
-  
-  rechazarPersona(persona: Persona) {
-    // Lógica para rechazar la persona
-  }
 
   constructor(public dialog: MatDialog, private personaService: UsuarioService) {}
-  
-
-  imagen(){
-    Swal.fire({
-      html: '<img src="assets/data/comprobante.png">'
-
-    }
-      
-    )
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
   
-  
 
-  mostrarAlerta(){
-    Swal.fire(
-      'Pago Aceptado',
-      'Aceptado',
-      'success'
-    )
-  }
-
-  errorAlerta(){
-    Swal.fire({
-      icon: 'error',
-      title: 'No es Correcto',
-      text: 'Enviado Correo Electrónico',
-      footer: '<a href="">@RentaAutos</a>'
-    })
-  }
-  
 
 }

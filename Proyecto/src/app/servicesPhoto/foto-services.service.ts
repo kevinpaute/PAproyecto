b7f2b5e55eefe5e82a3889c369769a84a1f9fb73
Jhon Zambrano Macias <75482142+JhonZambranoM@users.noreply.github.com>
2023-08-30 22:50:02 -0500

    Creación método enviar foto

--- Proyecto/src/app/servicesPhoto/foto-services.service.ts
+++ Proyecto/src/app/servicesPhoto/foto-services.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class FotoServicesService {
 
+  URL='http://localhost:4000/api/photos'
+  id: string | undefined; // Define la propiedad usuarioId
+
   constructor() { }
+
+  createFoto(title:string, description:string,photo:File){
+
+    const fd =new FormData();
+    fd.append('title',title);
+    fd.append('description',description);
+    fd.append('image',photo);
+    return this.http.post(this.URL, fd);
+
+  }
+
 }
